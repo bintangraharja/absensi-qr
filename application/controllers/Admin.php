0ee16bf130ae5776408c2cb3ae6eb70389860991
Nan <anandasetiyani@gmail.com>
2022-12-10 23:00:42 +0700

    tabel admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -8,7 +8,9 @@ class Admin extends CI_Controller {
 		$this->load->library('Ciqrcode');
 	}
 	public function index(){
-		$data['title'] = "Absensi QR";
+		$data['title'] = "CIMB Niaga SAT 2022";
+		$data['style'] = $this->load->view('include/style.php', NULL, TRUE);
+		$data['script'] = $this->load->view('include/script.php', NULL, TRUE);
 		$this->load->view('admin' , $data);
 	}
 	public function QRcode($kodenya){
--- application/views/admin.php
+++ application/views/admin.php
@@ -4,38 +4,50 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title><?=$title?></title>
-	<link rel="stylesheet" href="">
+	<?= $style ?>
+	<?= $script ?>
 </head>
 <body>
-	<h1><?=$title?> </h1>
-	<table>
-		<thead>
-			<tr>
-				<th>No</th>
-				<th>Name</th>
-				<th>Phone Number</th>
-				<th>QR Barcode</th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php
-		for($i = 0; $i < 5; $i++){
-			$kode = 'Quiz'.$i;
-			$name = "Bintang";
-			$phone = "08980444007";
-		?>
-			<tr>
-				<td><?=$i;?></td>
-				<td><?=$name;?></td>
-				<td><?=$phone;?></td>
-				<td><a href="<?php echo site_url('Admin/QRcode/'.$kode);?>" download="<?=$name;?>">
-						<button>Download</button>
-					</a>
-				</td>
-			</tr>
-		<?php }?>
-		</tbody>
-	</table>
-
+	<div class="container">
+		<h1 class="text-center" style="color: #550540;">Guest Book</h1>
+		<table class="table">
+			<thead class="thead-custom text-center">
+				<tr>
+					<th scope="col">ID</th>
+					<th scope="col">Full Name</th>
+					<th scope="col">Email</th>
+					<th scope="col">Phone Number</th>
+					<th scope="col">Status</th>
+					<th scope="col">Barcode</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php
+				for($i = 100; $i < 250; $i++){
+					$kode = 'Quiz'.$i;
+					$name = "Bintang";
+					$email = "bintang@gmail.com";
+					$phone = "08980444007";
+					$status = FALSE;
+			?>
+				<tr class="text-center">
+					<td><?=$i;?></td>
+					<td><?=$name;?></td>
+					<td><?=$email;?></td>
+					<td><?=$phone;?></td>
+					<td><label class="switch">
+							<input type="checkbox" <?php if($status) {?> checked <?php }?> disabled>
+							<span class="slider round"></span>
+						</label>
+					</td>
+					<td><a href="<?php echo site_url('Admin/QRcode/'.$kode);?>" download="<?=$name;?>">
+							<button>Download</button>
+						</a>
+					</td>
+				</tr>
+			<?php }?>
+			</tbody>
+		</table>
+	</div>
 </body>
 </htmL>
